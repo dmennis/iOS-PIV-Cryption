2143d91b1333ffd391424d77f9d052b716208de9
Dennis Hills <dennis.hills@yubico.com>
2024-12-12 00:29:48 -0800

    initial commit of pivcryption test demo for piv encryption/decryption

---
+++ PIVCryption/PIVCryptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIVCryptionApp.swift
+//  PIVCryption
+//
+//  Created by Dennis Hills on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PIVCryptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
